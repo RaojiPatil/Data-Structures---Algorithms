50ceee9f49d1bc32de46bd784b145a2d8a6f9d74
Raoji Patil <raojipatil22@gmail.com>
2022-02-20 23:06:19 +0530

    dsa

---
+++ 1_Array/..practies/Jump count.js
@@ -0,0 +1,67 @@
+var c = [0,0,1,0,0,1,0];
+
+function jumpingOnClouds(c) {
+    var jumpCount = 0;
+    let step = 0;
+    for(let i=0; i<c.length; i){
+if(c[i] === 0 && i !== (c.length - 2)){
+    i = i + 2;
+    jumpCount++
+    } else if (c[i] === 0 && i === (c.length - 2)){
+    i = i + 1;
+    jumpCount++
+    } else {
+    i = i - 1;
+    }
+    }
+    return jumpCount - 1;
+    }
+
+    console.log(jumpingOnClouds(c));
+
+
+
+    function jumpingOnClouds(c){
+        let numOfJumps = 0;
+        
+        for(let i = 0; i < c.length; i++){
+        if(c[i] === 0 && c[i + 2] === 0){
+        numOfJumps = numOfJumps + 1;
+        i = i + 1;
+        continue;
+        }
+        if(c[i] === 0 && c[i+1] === 0 && c[i+2] === 1){
+        numOfJumps = numOfJumps + 1;
+        continue;
+        }
+        if(c[i] === 0 && c[i+1] === 0 && !c[i+2]){
+        numOfJumps = numOfJumps + 1;
+        continue;
+        }
+        }
+        
+        return numOfJumps;
+        }
+
+
+
+
+var c = [0,0,1,0,0,1,0];
+var count = 0;
+var step = 0;
+
+for(var i = 0; i < c.length; i) {
+    if(c[i] == 0 && c[i+1] == 0 && c[i+2] == 0) {
+        i = i+2;
+        count++;
+    } else if(c[i] == 0 && c[i+1] == 0 && c[i+2] !== 0) {
+        i++;
+        count++;
+    }
+    i++;
+}
+console.log(count);
+
+
+
+
